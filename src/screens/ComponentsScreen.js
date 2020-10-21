ac565f5e52e7a01d3594b73a231f8e1de43d1eb9
Shiv <shivakp2111@gmail.com>
2020-10-21 21:09:58 +0530

    multiple style attributes

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 // Components
 const ComponentsScreen = () => {
-    return <Text style={defaultStyle.textStyle}>This is a components screen</Text>;
-}
+    return (
+        <View style={defaultStyle.paddingTop}>
+            <Text style={defaultStyle.textStyle}>This is a components screen</Text>
+            <Text style={Object.assign({}, defaultStyle.textStyle, defaultStyle.paddingTop)}>Hi there!</Text>
+        </View>
+    );
+};
 
 // Stylesheet
 const defaultStyle = StyleSheet.create({
-    textStyle:{
+    textStyle: {
         fontSize: 30
     },
     red: {
         color: '#9C27B0'
+    },
+    paddingTop: {
+        paddingTop: 10
+    },
+    paddingLeft: {
+        paddingLeft: 10
+    },
+    paddingRight: {
+        paddingRight: 10
+    },
+    paddingBottom: {
+        paddingBottom: 10
     }
 });
 
