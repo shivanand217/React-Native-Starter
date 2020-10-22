4ca1c2f6a25ee7e750cc06eb02c4673105d56150
Shiv <shivakp2111@gmail.com>
2020-10-22 15:48:19 +0530

    added keyExtractor for FlatList

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { View, Text, Stylesheet, FlatList } from 'react-native';
 
 
 // Components
 const ListScreen = () => {
-    return <Text> This is a List Screen </Text>
+    const friends = [
+        {name: 'Friend #1'},
+        {name: 'Friend #2'},
+        {name: 'Friend #3'}
+    ];
+
+    // Flat list required data prop and renderItem to render a list
+
+    return (
+        <FlatList 
+            keyExtractor={friend => friend.name}
+            data={friends} 
+            renderItem={({ item }) => {
+                return <Text> {item.name} </Text>
+            }} 
+        />
+    )
 };
 
-const styles = Stylesheet.create({
+// const styles = Stylesheet.create({
 
-});
+// });
 
 // export this component
 export default ListScreen;
